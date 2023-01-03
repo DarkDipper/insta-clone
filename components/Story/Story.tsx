fcdd2260f95ac3f0ae037b7dde4f3d85b88e0295
Phong <phong.huynh@weai.vn>
2023-01-03 21:11:22 +0700

    Improve story component

--- components/Story/Story.tsx
+++ components/Story/Story.tsx
@@ -8,48 +8,24 @@ export default function Story() {
       elementScroll.current.scrollLeft += scrollOffset;
     }
   };
+  let list_temp = [];
+  for (let i = 0; i <= 10; i++) {
+    list_temp.push(
+      <StoryAvatar
+        key={i}
+        src="https://i.ibb.co/YXL10VM/animelody.png"
+        userName="Hero Shield fwqfwqfwqffwqfqwfw"
+        haveSeenBefore={false}
+      />
+    );
+  }
   return (
     <>
       <div className="story" ref={elementScroll}>
         <button onClick={() => handleScroll(-400)} className="btn-scroll-left">
           <AiFillLeftCircle />
         </button>
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
-        <StoryAvatar
-          src="https://i.ibb.co/YXL10VM/animelody.png"
-          userName="Hero Shield fwqfwqfwqffwqfqwfw"
-        />
+        {list_temp}
         <button onClick={() => handleScroll(400)} className="btn-scroll-right">
           <AiFillRightCircle />
         </button>
--- components/StoryAvatar/StoryAvatar.tsx
+++ components/StoryAvatar/StoryAvatar.tsx
@@ -3,26 +3,33 @@ import Avatar from "../Avatar";
 type Props = {
   src: string;
   userName: string;
+  haveSeenBefore: boolean;
+  width?: number;
+  height?: number;
 };
 
-export default function StoryAvatar({ src, userName }: Props) {
+export default function StoryAvatar({
+  src,
+  userName,
+  haveSeenBefore,
+  width = 56,
+  height = 56,
+}: Props) {
   return (
     <div className="story-avatar">
       <div className="story-avatar__wrapper">
         <div className="story-avatar__avatar">
-          <div className="story-avatar__avatar__gradient-border">
-            <div className="story-avatar__avatar__white-border">
-              <Avatar
-                src="https://i.ibb.co/YXL10VM/animelody.png"
-                height={56}
-                width={56}
-              />
+          <div
+            className={`story-avatar__avatar${
+              !haveSeenBefore ? "--gradient-border" : "--gray-border"
+            }`}
+          >
+            <div className="story-avatar__avatar--white-border">
+              <Avatar src={src} height={height} width={width} />
             </div>
           </div>
         </div>
-        <p className="story-avatar--user-name">
-          Hero Shield fwqfwqfwqffwqfqwfw
-        </p>
+        <p className="story-avatar--user-name">{userName}</p>
       </div>
     </div>
   );
